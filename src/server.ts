43248b3bc5589c54035d6b8ea38a2dd5c98defde
TN0123 <112415698+TN0123@users.noreply.github.com>
2025-02-19 11:58:19 -0500

    Fixed some variables for deployment, changed cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express from "express";
 import session from "express-session";
 import dotenv from "dotenv";
-import bcrypt from "bcryptjs";
 import cors from "cors";
 import user from "./routes/user";
 import ai from "./routes/ai";
 import usersession from "./routes/session";
-import mongoose from "mongoose";
-import UserModel from "./models/UserModel";
-import * as sessionTypes from "./types/session";
 
 dotenv.config();
 
@@ -28,7 +24,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.EXPO_PUBLIC_BASE_URL,
+    origin: "*",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
@@ -41,6 +37,6 @@ app.use("/ai", ai);
 app.use("/session", usersession);
 
 //change host to 0.0.0.0 when deploying, localhost for test
-app.listen({ port: process.env.EXPO_PUBLIC_PORT, host: "0.0.0.0" }, () =>
-  console.log("Server running on port: " + process.env.EXPO_PUBLIC_PORT)
+app.listen({ port: process.env.PORT, host: "0.0.0.0" }, () =>
+  console.log("Server running on port: " + process.env.PORT)
 );
